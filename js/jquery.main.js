8339030896d19654868585cb5b0f6baf9ac8fd1e
Shutko Dmitry <ds@proverstka.com>
2016-08-26 12:00:50 +0300

    create popup

--- js/jquery.main.js
+++ js/jquery.main.js
@@ -6,4 +6,43 @@ $(document).ready(function(){
 	$('.how-work-list').bxSlider({
 		pagerCustom: '#bx-pager'
 	});
+	// $('.doctor-list').bxSlider({
+	// 	minSlides: 3,
+	// 	maxSlides: 3,
+	// 	slideWidth: 240,
+	// 	slideMargin: 10
+	// });
+	$('.doctor-list-slider').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+			    slidesToShow: 3,
+			    slidesToScroll: 3,
+			    infinite: true,
+			    dots: true
+			  }
+			},
+			{
+			  breakpoint: 600,
+			  settings: {
+			    slidesToShow: 2,
+			    slidesToScroll: 2
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+			    slidesToShow: 1,
+			    slidesToScroll: 1
+			  }
+			}
+		]
+	});
+	// select
+	$(".custom-select select").dropdown();
 });
